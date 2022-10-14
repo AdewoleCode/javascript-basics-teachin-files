1004b2674a40b2ad9832976b901f6e702b620af4
ademola <adeadewole1712@gmail.com>
2022-10-14 12:14:37 +0100

    traversing the dom

--- jav2/app.js
+++ jav2/app.js
@@ -12,7 +12,7 @@ console.log(items[2].textContent)
 // secondChild.textContent ='second element'
 
 items[0].textContent = 'hello there'
-items[1].style.fontSize = '50px'
+// items[1].style.fontSize = '50px'
 items[3].style.backgroundColor = 'red'
 
 // pAll = document.querySelectorAll('p')
@@ -29,6 +29,24 @@ input.value = 'hello world'
 
 const odd = document.querySelectorAll('.item:nth-child(odd)')
 
-for (let i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = 'blue'
-}
+// for (let i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = 'blue'
+// }
+
+let lists = document.querySelector('.ul')
+console.log(lists.parentElement)
+lists.parentElement.style.backgroundColor = 'red'
+
+console.log(lists.children[2])
+
+console.log(lists.lastElementChild)
+
+const newLi = document.createElement('li')
+
+lists.append(newLi)
+
+newLi.textContent = 'new list item'
+newLi.style.fontSize = '30px'
+
+
+
